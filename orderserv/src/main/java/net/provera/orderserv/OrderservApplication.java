ddf7a3c867352323d34ce053849031cad3504e7c
Yusuf Sahin <yusuf@yusufsahin.net>
2023-04-08 00:34:12 +0300

    docker-compose.yml update

---
+++ orderserv/src/main/java/net/provera/orderserv/OrderservApplication.java
@@ -0,0 +1,13 @@
+package net.provera.orderserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderservApplication.class, args);
+    }
+
+}
---
+++ orderserv/src/test/java/net/provera/orderserv/OrderservApplicationTests.java
@@ -0,0 +1,13 @@
+package net.provera.orderserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderservApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
